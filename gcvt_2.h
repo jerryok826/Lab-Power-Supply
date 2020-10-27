61e065e53e473404acaed8f4a81ab728512f18a2
Jerry Okeefe <jerry@ologicinc.com>
2020-10-27 15:10:02 -0700

    add hardware projects

--- gcvt_2.h
+++
@@ -1,6 +0,0 @@
-#ifndef __GCVT_2_H
-#define __GCVT_2_H
-
-char *gcvt_2 (float f, uint16_t ndigit, char *buf);
-
-#endif
--- modbus_drv.h
+++
@@ -1,7 +0,0 @@
-#ifndef __MODBUS_DRV_H
-#define __MODBUS_DRV_H
-
-void init_registers (void);
-int modbus_process (char *rx_buf, int mosbua_pkt_len, char *tx_buf);
-
-#endif /* __MODBUS_H */
